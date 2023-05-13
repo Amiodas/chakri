2d2a77c84a4010c55291045b36b0d313246ea274
Amiodas <amio.das.dip@gmail.com>
2023-05-13 17:24:40 +0100

    #initial commit, #Navbar section  added, #Banner section  added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto">
+        <Header></Header>
+        <Banner></Banner>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+function Banner() {
+  return (
+    <div className="container mx-auto px-5 flex py-8">
+      <div className="flex-auto">
+        <h1 className="text-6xl font-bold">
+          One Step <br /> Closer To Your <br />
+          <span className="text-indigo-600">Dream Job</span>
+        </h1>
+        <p className="text-slate-600 pt-8">
+          Explore thousands of job opportunities with all the information <br /> you
+          need. Its your future. Come find it. Manage all your job <br /> application
+          from start to finish.
+        </p>
+        <button className="bg-indigo-600 p-3 mt-5 text-white hover:rounded">
+          Get Started
+        </button>
+      </div>
+      <div className="flex justify-end flex-auto">
+        <img className="w-auto h-96" src="/src/assets/assets/All Images/P3OLGJ1 copy 1.png" />
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
