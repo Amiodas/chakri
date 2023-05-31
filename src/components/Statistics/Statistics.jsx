8d80a818813bbceb8a558e1dd457df50f7cf2f82
Amiodas <amio.das.dip@gmail.com>
2023-05-31 21:20:38 +0100

    #statistics chart implemented

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,348 +1,51 @@
-import { Treemap } from "recharts";
+import { Legend, RadialBar, RadialBarChart, Tooltip, Treemap } from "recharts";
 
 function Statistics() {
   const data = [
     {
-      name: "axis",
-      children: [
-        {
-          name: "Axis",
-          size: 24593,
-        },
-        {
-          name: "Axes",
-          size: 1302,
-        },
-        {
-          name: "AxisGridLine",
-          size: 652,
-        },
-        {
-          name: "AxisLabel",
-          size: 636,
-        },
-        {
-          name: "CartesianAxes",
-          size: 6703,
-        },
-      ],
+      name: "Assignment 1",
+      mark: 57,
+      pv: 2400,
+      fill: "#8884d8",
     },
     {
-      name: "controls",
-      children: [
-        {
-          name: "TooltipControl",
-          size: 8435,
-        },
-        {
-          name: "SelectionControl",
-          size: 7862,
-        },
-        {
-          name: "PanZoomControl",
-          size: 5222,
-        },
-        {
-          name: "HoverControl",
-          size: 4896,
-        },
-        {
-          name: "ControlList",
-          size: 4665,
-        },
-        {
-          name: "ClickControl",
-          size: 3824,
-        },
-        {
-          name: "ExpandControl",
-          size: 2832,
-        },
-        {
-          name: "DragControl",
-          size: 2649,
-        },
-        {
-          name: "AnchorControl",
-          size: 2138,
-        },
-        {
-          name: "Control",
-          size: 1353,
-        },
-        {
-          name: "IControl",
-          size: 763,
-        },
-      ],
+      name: "Assignment 2",
+      mark: 59,
+      pv: 4567,
+      fill: "#83a6ed",
     },
     {
-      name: "data",
-      children: [
-        {
-          name: "Data",
-          size: 20544,
-        },
-        {
-          name: "NodeSprite",
-          size: 19382,
-        },
-        {
-          name: "DataList",
-          size: 19788,
-        },
-        {
-          name: "DataSprite",
-          size: 10349,
-        },
-        {
-          name: "EdgeSprite",
-          size: 3301,
-        },
-        {
-          name: "render",
-          children: [
-            {
-              name: "EdgeRenderer",
-              size: 5569,
-            },
-            {
-              name: "ShapeRenderer",
-              size: 2247,
-            },
-            {
-              name: "ArrowType",
-              size: 698,
-            },
-            {
-              name: "IRenderer",
-              size: 353,
-            },
-          ],
-        },
-        {
-          name: "ScaleBinding",
-          size: 11275,
-        },
-        {
-          name: "TreeBuilder",
-          size: 9930,
-        },
-        {
-          name: "Tree",
-          size: 7147,
-        },
-      ],
+      name: "Assignment 3",
+      mark: 57,
+      pv: 1398,
+      fill: "#8dd1e1",
     },
     {
-      name: "events",
-      children: [
-        {
-          name: "DataEvent",
-          size: 7313,
-        },
-        {
-          name: "SelectionEvent",
-          size: 6880,
-        },
-        {
-          name: "TooltipEvent",
-          size: 3701,
-        },
-        {
-          name: "VisualizationEvent",
-          size: 2117,
-        },
-      ],
+      name: "Assignment 4",
+      mark: 52,
+      pv: 9800,
+      fill: "#82ca9d",
     },
     {
-      name: "legend",
-      children: [
-        {
-          name: "Legend",
-          size: 20859,
-        },
-        {
-          name: "LegendRange",
-          size: 10530,
-        },
-        {
-          name: "LegendItem",
-          size: 4614,
-        },
-      ],
+      name: "Assignment 5",
+      mark: 43,
+      pv: 3908,
+      fill: "#a4de6c",
     },
     {
-      name: "operator",
-      children: [
-        {
-          name: "distortion",
-          children: [
-            {
-              name: "Distortion",
-              size: 6314,
-            },
-            {
-              name: "BifocalDistortion",
-              size: 4461,
-            },
-            {
-              name: "FisheyeDistortion",
-              size: 3444,
-            },
-          ],
-        },
-        {
-          name: "encoder",
-          children: [
-            {
-              name: "PropertyEncoder",
-              size: 4138,
-            },
-            {
-              name: "Encoder",
-              size: 4060,
-            },
-            {
-              name: "ColorEncoder",
-              size: 3179,
-            },
-            {
-              name: "SizeEncoder",
-              size: 1830,
-            },
-            {
-              name: "ShapeEncoder",
-              size: 1690,
-            },
-          ],
-        },
-        {
-          name: "filter",
-          children: [
-            {
-              name: "FisheyeTreeFilter",
-              size: 5219,
-            },
-            {
-              name: "VisibilityFilter",
-              size: 3509,
-            },
-            {
-              name: "GraphDistanceFilter",
-              size: 3165,
-            },
-          ],
-        },
-        {
-          name: "IOperator",
-          size: 1286,
-        },
-        {
-          name: "label",
-          children: [
-            {
-              name: "Labeler",
-              size: 9956,
-            },
-            {
-              name: "RadialLabeler",
-              size: 3899,
-            },
-            {
-              name: "StackedAreaLabeler",
-              size: 3202,
-            },
-          ],
-        },
-        {
-          name: "layout",
-          children: [
-            {
-              name: "RadialTreeLayout",
-              size: 12348,
-            },
-            {
-              name: "NodeLinkTreeLayout",
-              size: 12870,
-            },
-            {
-              name: "CirclePackingLayout",
-              size: 12003,
-            },
-            {
-              name: "CircleLayout",
-              size: 9317,
-            },
-            {
-              name: "TreeMapLayout",
-              size: 9191,
-            },
-            {
-              name: "StackedAreaLayout",
-              size: 9121,
-            },
-            {
-              name: "Layout",
-              size: 7881,
-            },
-            {
-              name: "AxisLayout",
-              size: 6725,
-            },
-            {
-              name: "IcicleTreeLayout",
-              size: 4864,
-            },
-            {
-              name: "DendrogramLayout",
-              size: 4853,
-            },
-            {
-              name: "ForceDirectedLayout",
-              size: 8411,
-            },
-            {
-              name: "BundledEdgeRouter",
-              size: 3727,
-            },
-            {
-              name: "IndentedTreeLayout",
-              size: 3174,
-            },
-            {
-              name: "PieLayout",
-              size: 2728,
-            },
-            {
-              name: "RandomLayout",
-              size: 870,
-            },
-          ],
-        },
-        {
-          name: "OperatorList",
-          size: 5248,
-        },
-        {
-          name: "OperatorSequence",
-          size: 4190,
-        },
-        {
-          name: "OperatorSwitch",
-          size: 2581,
-        },
-        {
-          name: "Operator",
-          size: 2490,
-        },
-        {
-          name: "SortOperator",
-          size: 2023,
-        },
-      ],
+      name: "Assignment 6",
+      mark: 60,
+      pv: 4800,
+      fill: "#d0ed57",
+    },
+    {
+      name: "Assignment 7",
+      mark: 55,
+      pv: 4800,
+      fill: "#ffc658",
     },
   ];
+
   return (
     <div className="container mx-auto px-16 pt-4 pb-10">
       <div className="bg-slate-100 py-16 rounded-lg">
@@ -350,16 +53,35 @@ function Statistics() {
           Statistics
         </h1>
       </div>
-      <div className="my-10 flex items-center justify-center">
-        <Treemap
-          width={730}
-          height={250}
-          data={data}
-          dataKey="size"
-          aspectRatio={4 / 3}
-          stroke="#fff"
-          fill="#4f46e5"
-        />
+      <div className="my-10">
+        <div className="flex items-center justify-center">
+          <RadialBarChart
+            width={600}
+            height={300}
+            innerRadius="20%"
+            outerRadius="100%"
+            data={data}
+            startAngle={360}
+            endAngle={0}
+          >
+            <RadialBar
+              minAngle={15}
+              label={{ fill: "#666", position: "insideStart" }}
+              background
+              clockWise={true}
+              dataKey="mark"
+            />
+            <Legend
+              iconSize={10}
+              width={120}
+              height={140}
+              layout="horizontal"
+              verticalAlign="middle"
+              align="left"
+            />
+            <Tooltip />
+          </RadialBarChart>
+        </div>
       </div>
     </div>
   );
