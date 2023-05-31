0af68d3e02bf41d4c5fcb501e1086a2d1834cb03
Amiodas <amio.das.dip@gmail.com>
2023-05-31 23:04:24 +0100

    #filter applied jobs functionality added

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,9 +1,28 @@
 import { useLoaderData } from "react-router-dom";
 import AppliedJob from "../AppliedJob/AppliedJob";
+import { useEffect, useState } from "react";
 
 function AppliedJobs() {
-  const appliedJobs = useLoaderData();
-  console.log(appliedJobs);
+  const loadJobs = useLoaderData();
+  const [appliedJobs, setAppliedJobs] = useState([]);
+
+  useEffect(() => {
+    setAppliedJobs(loadJobs);
+  }, []);
+
+  const handleFilterRemoteJobs = () => {
+    const filterData = loadJobs.filter((item) =>
+      item.jobTypes.includes("Remote")
+    );
+    setAppliedJobs(filterData);
+  };
+
+  const handleFilterOnsiteJobs = () => {
+    const filterData = loadJobs.filter((item) =>
+      item.jobTypes.includes("Onsite")
+    );
+    setAppliedJobs(filterData);
+  };
   return (
     <div className="container mx-auto px-16 pt-4 pb-10">
       <div className="bg-slate-100 py-16 rounded-lg">
@@ -11,16 +30,22 @@ function AppliedJobs() {
           Applied jobs
         </h1>
       </div>
-      
-      {appliedJobs.length === 0 ? (
+
+      {appliedJobs?.length === 0 ? (
         <p className="my-10 text-center">No Applied jobs yet.</p>
       ) : (
         <div className="my-10">
           <div className="text-right mb-5">
-            <button className="px-6 py-2 bg-slate-100 border-2 rounded text-xs font-semibold text-slate-400">
+            <button
+              onClick={handleFilterRemoteJobs}
+              className="px-6 py-2 bg-slate-100 border-2 rounded text-xs font-semibold text-slate-400"
+            >
               Remote Job
             </button>
-            <button className="ml-3 px-6 py-2 bg-slate-100 border-2 rounded text-xs font-semibold text-slate-400">
+            <button
+              onClick={handleFilterOnsiteJobs}
+              className="ml-3 px-6 py-2 bg-slate-100 border-2 rounded text-xs font-semibold text-slate-400"
+            >
               Onsite Job
             </button>
           </div>
